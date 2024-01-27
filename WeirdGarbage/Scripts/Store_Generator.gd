4e14ad863123f8b306c1c94690fea6129e6fddd5
ButtFungus <72562459+ButtFungus@users.noreply.github.com>
2024-01-26 22:50:15 -0500

    Updating Store Generator for future variations

--- WeirdGarbage/Scripts/Store_Generator.gd
+++ WeirdGarbage/Scripts/Store_Generator.gd
@@ -2,20 +2,34 @@ extends Spatial
 
 #Store Chunk segments
 var Chunk_Center_0 = preload("res://Scenes/Store/StoreChunk_Center_0.tscn")
-var Chunk_Corner_BL = preload("res://Scenes/Store/StoreChunk_Corner_BL.tscn")
-var Chunk_Corner_BR = preload("res://Scenes/Store/StoreChunk_Corner_BR.tscn")
-var Chunk_Corner_TL = preload("res://Scenes/Store/StoreChunk_Corner_TL.tscn")
-var Chunk_Corner_TR = preload("res://Scenes/Store/StoreChunk_Corner_TR.tscn")
-var Chunk_Wall_B = preload("res://Scenes/Store/StoreChunk_Wall_B.tscn")
-var Chunk_Wall_L = preload("res://Scenes/Store/StoreChunk_Wall_L.tscn")
-var Chunk_Wall_R = preload("res://Scenes/Store/StoreChunk_Wall_R.tscn")
-var Chunk_Wall_T = preload("res://Scenes/Store/StoreChunk_Wall_T.tscn")
+var Chunk_Corner_BL_0 = preload("res://Scenes/Store/StoreChunk_Corner_BL.tscn")
+var Chunk_Corner_BR_0 = preload("res://Scenes/Store/StoreChunk_Corner_BR.tscn")
+var Chunk_Corner_TL_0 = preload("res://Scenes/Store/StoreChunk_Corner_TL.tscn")
+var Chunk_Corner_TR_0 = preload("res://Scenes/Store/StoreChunk_Corner_TR.tscn")
+var Chunk_Wall_B_0 = preload("res://Scenes/Store/StoreChunk_Wall_B.tscn")
+var Chunk_Wall_L_0 = preload("res://Scenes/Store/StoreChunk_Wall_L.tscn")
+var Chunk_Wall_R_0 = preload("res://Scenes/Store/StoreChunk_Wall_R.tscn")
+var Chunk_Wall_T_0 = preload("res://Scenes/Store/StoreChunk_Wall_T.tscn")
+
+var Chunk_Directory = {
+	"Center":[Chunk_Center_0],
+	"Corner_BL":[Chunk_Corner_BL_0],
+	"Corner_BR":[Chunk_Corner_BR_0],
+	"Corner_TL":[Chunk_Corner_TL_0],
+	"Corner_TR":[Chunk_Corner_TR_0],
+	"Wall_B":[Chunk_Wall_B_0],
+	"Wall_L":[Chunk_Wall_L_0],
+	"Wall_R":[Chunk_Wall_R_0],
+	"Wall_T":[Chunk_Wall_T_0]
+}
 
 onready var Chunk_Holder = get_node("LayoutHolder")
 
 const Store_Size_Min = 2#DO NOT CHANGE THIS should be 2 always
-var Store_Size_X_Max = 3
-var Store_Size_Z_Max = 3
+var Store_Size_X_Max = 5#Max horizontal size of the Store
+var Store_Size_Z_Max = 5#Max depth size of the Store
+var Store_Size_X = 0
+var Store_Size_Z = 0
 
 
 func RandomNum(type,range1,range2):#Creates Random Numbers
@@ -32,38 +46,51 @@ func RandomNum(type,range1,range2):#Creates Random Numbers
 func CreateChunk(ChunkParent,Xpos,Zpos,ChunkInstance):#Creates store chunks and places them
 	var NewChunk = ChunkInstance.instance()
 	ChunkParent.add_child(NewChunk)
-	NewChunk.transform.origin = Vector3(Xpos,0,Zpos)*2.10
+	NewChunk.transform.origin = Vector3(Xpos,0,Zpos)*2.0
 
 
 func GenerateStore(StoreHolder):#Generates a store with double for loops yo
-	StoreHolder.transform.origin.x = -(Store_Size_X_Max-1)
-	StoreHolder.transform.origin.z = -(Store_Size_Z_Max-1)
-	for x in range(0,max(Store_Size_X_Max,Store_Size_Min)):
-		for z in range(0,max(Store_Size_Z_Max,Store_Size_Min)):
-			var ChunkToCreate = Chunk_Center_0
+	StoreHolder.transform.origin.x = -(Store_Size_X-1)
+	StoreHolder.transform.origin.z = -(Store_Size_Z-1)
+	for x in range(0,max(Store_Size_X,Store_Size_Min)):
+		for z in range(0,max(Store_Size_Z,Store_Size_Min)):
+			var ChunkToCreate = Chunk_Directory["Center"][0]
 			if x == 0:
 				if z == 0:
-					ChunkToCreate = Chunk_Corner_TL
-				elif z == Store_Size_Z_Max-1:
-					ChunkToCreate = Chunk_Corner_BL
+					ChunkToCreate = Chunk_Directory["Corner_TL"][RandomNum("int",0,len(Chunk_Directory["Corner_TL"])-1)]
+				elif z == Store_Size_Z-1:
+					ChunkToCreate = Chunk_Directory["Corner_BL"][RandomNum("int",0,len(Chunk_Directory["Corner_BL"])-1)]
 				else:
-					ChunkToCreate = Chunk_Wall_L
-			elif x == Store_Size_X_Max-1:
+					ChunkToCreate = Chunk_Directory["Wall_L"][RandomNum("int",0,len(Chunk_Directory["Wall_L"])-1)]
+			elif x == Store_Size_X-1:
 				if z == 0:
-					ChunkToCreate = Chunk_Corner_TR
-				elif z == Store_Size_Z_Max-1:
-					ChunkToCreate = Chunk_Corner_BR
+					ChunkToCreate = Chunk_Directory["Corner_TR"][RandomNum("int",0,len(Chunk_Directory["Corner_TR"])-1)]
+				elif z == Store_Size_Z-1:
+					ChunkToCreate = Chunk_Directory["Corner_BR"][RandomNum("int",0,len(Chunk_Directory["Corner_BR"])-1)]
 				else:
-					ChunkToCreate = Chunk_Wall_R
+					ChunkToCreate = Chunk_Directory["Wall_R"][RandomNum("int",0,len(Chunk_Directory["Wall_R"])-1)]
 			else:
 				if z == 0:
-					ChunkToCreate = Chunk_Wall_T
-				elif z == Store_Size_Z_Max-1:
-					ChunkToCreate = Chunk_Wall_B
+					ChunkToCreate = Chunk_Directory["Wall_T"][RandomNum("int",0,len(Chunk_Directory["Wall_T"])-1)]
+				elif z == Store_Size_Z-1:
+					ChunkToCreate = Chunk_Directory["Wall_B"][RandomNum("int",0,len(Chunk_Directory["Wall_B"])-1)]
 				else:
-					ChunkToCreate = Chunk_Center_0
+					ChunkToCreate = Chunk_Directory["Center"][RandomNum("int",0,len(Chunk_Directory["Center"])-1)]
 			CreateChunk(StoreHolder,x,z,ChunkToCreate)
 
 
 func _ready():
+	Store_Size_X = RandomNum("int",Store_Size_Min,Store_Size_X_Max)
+	Store_Size_Z = RandomNum("int",Store_Size_Min,Store_Size_Z_Max)
 	GenerateStore(Chunk_Holder)#Create the store!
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Test_Store_Generate"):
+		print("Regenerating Store...")
+		if Chunk_Holder.get_child_count() > 0:
+			for child in Chunk_Holder.get_children():
+				child.queue_free()
+		Store_Size_X = RandomNum("int",Store_Size_Min,Store_Size_X_Max)
+		Store_Size_Z = RandomNum("int",Store_Size_Min,Store_Size_Z_Max)
+		GenerateStore(Chunk_Holder)#Create the store!
