a877846d5342b61d85105c10eff1d9f7300abd54
James Marcil <james@jamesmarcil.com>
2024-01-27 10:30:01 -0500

    Riffing on movement code.

--- WeirdGarbage/Scripts/character_controls.gd
+++ WeirdGarbage/Scripts/character_controls.gd
@@ -3,11 +3,11 @@ extends KinematicBody
 class_name CharacterController
 
 export(float) var tilt_acceleration:float
-export(Vector3) var max_velocity:Vector3
-export(Vector3) var max_acceleration:Vector3
+export(float) var tilt_deceleration:float
+export(float) var max_velocity:float
+export(float) var friction:float
 
 var velocity:Vector3 = Vector3.ZERO
-var acceleration:Vector3 = Vector3.ZERO
 
 static func vector3_max(a:Vector3, b:Vector3) -> Vector3:
 	var result:Vector3 = Vector3()
@@ -25,22 +25,44 @@ static func vector3_min(a:Vector3, b:Vector3) -> Vector3:
 	
 func _ready():
 	self.velocity = Vector3.ZERO
-	self.acceleration = Vector3.ZERO
 
 func _physics_process(delta:float) -> void:
 	if Input.is_action_pressed("lean_forward"):
-		acceleration += tilt_acceleration * Vector3.FORWARD * delta
+		if self.velocity.z < 0:
+			self.velocity.z += tilt_deceleration
+			if self.velocity.z >= 0:
+				self.velocity.z = tilt_deceleration 
+		elif self.velocity.z < max_velocity:
+			self.velocity.z += tilt_acceleration
+			self.velocity.z = min(self.velocity.z, max_velocity)			
 	elif Input.is_action_pressed("lean_backward"):
-		acceleration += tilt_acceleration * Vector3.BACK * delta
+		if self.velocity.z > 0:
+			self.velocity.z -= tilt_deceleration
+			if self.velocity.z <= 0:
+				self.velocity.z = -tilt_deceleration
+		elif self.velocity.z > -max_velocity:
+			self.velocity.z -= tilt_acceleration
+			self.velocity.z = max(self.velocity.z, -max_velocity)
+	else:
+		self.velocity.z -= min(abs(self.velocity.z), friction) * sign(self.velocity.z)
 	
 	if Input.is_action_pressed("lean_left"):
-		acceleration += tilt_acceleration * Vector3.LEFT * delta
+		if self.velocity.x < 0:
+			self.velocity.x += tilt_deceleration
+			if self.velocity.x >= 0:
+				self.velocity.x = tilt_deceleration 
+		elif self.velocity.x < max_velocity:
+			self.velocity.x += tilt_acceleration
+			self.velocity.x = min(self.velocity.x, max_velocity)			
 	elif Input.is_action_pressed("lean_right"):
-		acceleration += tilt_acceleration * Vector3.RIGHT * delta
-	
-	acceleration = vector3_min(acceleration, max_acceleration)
-	
-	velocity += acceleration * delta
-	velocity = vector3_min(velocity, max_velocity)
+		if self.velocity.x > 0:
+			self.velocity.x -= tilt_deceleration
+			if self.velocity.x <= 0:
+				self.velocity.x = -tilt_deceleration
+		elif self.velocity.x > -max_velocity:
+			self.velocity.x -= tilt_acceleration
+			self.velocity.x = max(self.velocity.x, -max_velocity)
+	else:
+		self.velocity.x -= min(abs(self.velocity.x), friction) * sign(self.velocity.x)
 	
 	velocity = self.move_and_slide(velocity)
