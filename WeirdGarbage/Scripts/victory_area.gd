a18fd3a8427e2beffd2656423192477e93e76f99
James Marcil <james@jamesmarcil.com>
2024-01-28 13:46:06 -0500

    Stub out code to show/hide UI overlay.

--- WeirdGarbage/Scripts/victory_area.gd
+++ WeirdGarbage/Scripts/victory_area.gd
@@ -4,10 +4,15 @@ export(String, FILE) var victoryScene:String
 
 func _ready():
 	self.connect("body_entered", self, "on_body_entered")
+	self.connect("body_exited", self, "on_body_exited")
 
 func on_body_entered(rhs:PhysicsBody) -> void:
 	if rhs is CharacterController:
 		if (rhs as CharacterController).has_all_snacks():
 			get_tree().change_scene(victoryScene)
 		else:
-			pass # TODO: Trigger UI overlay
+			pass # TODO: Show UI overlay
+
+func on_body_exited(rhs:PhysicsBody) -> void:
+	if rhs is CharacterController:
+		pass # TODO: Hide UI overlay
