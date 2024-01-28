dbcbce2928e0e2997dd7c2994967289f67b55528
ButtFungus <72562459+ButtFungus@users.noreply.github.com>
2024-01-28 13:57:17 -0500

    Final Updates to the Camera Filter

--- WeirdGarbage/Scripts/SecurityHud.gd
+++ WeirdGarbage/Scripts/SecurityHud.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+onready var RecordMarker = get_node("RecordMarker")
 onready var TimeStamp = get_node("TimeStamp")
 var MiliSeconds = 0.0
 
@@ -10,4 +10,8 @@ func _physics_process(delta):
 		MiliSeconds += delta*60.0
 	else:
 		MiliSeconds = 0.0
+		if RecordMarker.visible == false:
+			RecordMarker.visible = true
+		else:
+			RecordMarker.visible = false
 	TimeStamp.text = str(CurrentTime["hour"])+":"+str(CurrentTime["minute"])+":"+str(CurrentTime["second"])+":"+str(int(MiliSeconds))
